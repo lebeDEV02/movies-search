b262393e2f86988b95c8210fd54aaa1bddd0f88f
ledeDEV02 <vladimirlebedev00010@gmail.com>
2021-01-14 14:24:17 +0500

    added the ability to add new filmes which will sort by Alphabet

--- js/script.js
+++ js/script.js
@@ -20,22 +20,39 @@ const movieDB = {
 		"2012",
 		"Ла-ла лэнд",
 		"Я - легенда",
-		"Скотт Пилигрим против..."
+		"Скотт Пилигрим против...",
 	]
 };
 const ad = document.querySelector('.promo__adv'),
 	genre = document.querySelector('.promo__genre'),
 	bg = document.querySelector('.promo__bg'),
-	films = document.querySelectorAll('.promo__interactive-item');
-
+	listItems = document.querySelector('.promo__interactive-list'),
+	films = listItems.querySelectorAll('li'),
+	buttonTracker = document.querySelector("button");
 ad.remove();
-
 genre.textContent = "Драма";
-
 bg.style.backgroundImage = "url('img/bg.jpg')";
-
-const dataBase = films.forEach((item, i) => {
-	item.textContent = movieDB.movies.sort()[i];
-	item.before(i + 1);
+let inputFilm,
+	clearInputFilm,
+	moviesCurrentLenght = movieDB.movies.length;
+let dataBase = films.forEach((item, i) => {
+	item.innerHTML = `${i + 1} ${movieDB.movies.sort()[i]} ${"<div class='delete'></div>"}`;
 });
-
+const newFilm = buttonTracker.addEventListener('click', (e => {
+	e.preventDefault();
+	inputFilm = document.querySelector('.adding__input').value;
+	if (inputFilm.length > 21) {
+		inputFilm = `${inputFilm.slice(0, 20)}...`;
+	}
+	if (inputFilm !== "") {
+		clearInputFilm = document.querySelector('.adding__input').value = "";
+		if (document.querySelector('.favourite').checked) {
+			console.log('Добавляем любимый фильм...');
+			document.querySelector('.favourite').checked = false;
+		};
+		movieDB.movies[moviesCurrentLenght++] = inputFilm;
+		films.forEach((item, i) => {
+			item.innerHTML = `${i + 1} ${movieDB.movies.sort()[i]} ${"<div class='delete'></div>"}`;
+		});
+	}
+}));
